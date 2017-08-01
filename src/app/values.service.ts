ea1c682fd3d653d303381ef95fa376da690fc329
sercanKuliq <sercan.kulig@gmail.com>
2017-08-01 16:08:25 +0300

    Service work

---
+++ src/app/values.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ValuesService {
+
+  constructor() { }
+
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+
+  getValue() {
+    return this.servers.slice();
+  }
+
+}
